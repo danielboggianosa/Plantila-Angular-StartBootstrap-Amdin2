57a354328ae9759ed119ac80a6dd9fd75d7d8ffd
Daniel Boggiano <coach@danielboggiano.com>
2020-02-27 21:11:40 -0500

    iniciando el proyecto con StartBootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/public/login/login.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { RecoverComponent } from './components/public/recover/recover.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'login', pathMatch: 'full'},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'recover', component: RecoverComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { RecoverComponent } from './components/public/recover/recover.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { TopbarComponent } from './components/common/topbar/topbar.component';
+import { SidebarComponent } from './components/common/sidebar/sidebar.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { DatatableComponent } from './components/datatable/datatable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoverComponent,
+    LayoutComponent,
+    TopbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    DashboardComponent,
+    ProfileComponent,
+    ConfigurationComponent,
+    CalendarComponent,
+    DatatableComponent
   ],
   imports: [
     BrowserModule,
