0ae889f7180dff4273534eef33568431f8264903
Daniel Boggiano <dboggiano@itdata.com.pe>
2021-04-28 16:52:27 -0500

    test 2 url patch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://cuentas.aprendofeliz.org/wp-json/cuentas/v1/'
+  apiUrl: 'https://cuentas.aprendofeliz.org/wp-json/cuentas/v1/'
 };
